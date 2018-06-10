983aab69d2ba36d002b9cc320bcde653a5724a38
Eamon Walsh <ewalsh@termysequence.com>
2018-06-10 05:15:46 -0400

    More work on build

--- Makefile
+++ Makefile
@@ -4,16 +4,14 @@ CFLAGS ?= -g -O2
 LDFLAGS ?= -g -O2
 
 PREFIX ?= /usr/local
-LIBDIR ?= $(DESTDIR)$(PREFIX)/lib64
+MULTILIB ?= lib
+LIBDIR ?= $(DESTDIR)$(PREFIX)/$(MULTILIB)
 INCDIR ?= $(DESTDIR)$(PREFIX)/include
-
-NINJA ?= /usr/bin/ninja
-ARMABI ?= hard
 
 .PHONY: all install
 
 all:
-	./build.sh $(V8ARCH) "$(CFLAGS)" "$(LDFLAGS)" $(NINJA) $(ARMABI)
+	./build.sh $(V8ARCH) "$(CFLAGS)" "$(LDFLAGS)" $(ARMFP)
 
 install:
 	./install.sh $(V8ARCH) $(LIBDIR) $(INCDIR)
--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Arguments: buildarch cflags ldflags ninja armabi
+# Arguments: buildarch cflags ldflags armfp
 
 set -ex
 cd v8
 
 v8arch=${1:-x64}
-ninja=${4:-/usr/bin/ninja}
-armabi=${5:hard}
 
 v8conf="use_sysroot=false use_gold=false linux_use_bundled_binutils=false \
 is_component_build=true clang_use_chrome_plugins=false libcpp_is_static=true \
 v8_use_external_startup_data=false v8_target_cpu=\"$v8arch\" is_clang=false \
-arm_float_abi=\"$armabi\""
+arm_float_abi=\"$4\""
 
 SPLITOPTFLAGS=""
 for i in $2; do
@@ -34,7 +32,7 @@ rm -rf buildtools/linux64/gn*
 cp -a out/Release/gn buildtools/linux64/gn
 
 rm -rf depot_tools/ninja
-ln -s "$ninja" depot_tools/ninja
+ln -s $(which ninja) depot_tools/ninja
 
 export PATH=$PATH:$(pwd)/depot_tools
 CHROMIUM_BUILDTOOLS_PATH=./buildtools/ gn gen out.gn/$v8arch.release --args="$v8conf"
